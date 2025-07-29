2263afc3dd4dc1c113be36d0a59ab95cb2bbbdac
Ujjwal jain <f20230602@goa.bits-pilani.ac.in>
2025-07-29 17:34:47 +0530

    made some edits for render

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/", router);
 
 connectDB().then(() => {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
