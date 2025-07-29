5266dae46d1b07c78a02ae5125f73d58e8e26dd9
Ujjwal jain <f20230602@goa.bits-pilani.ac.in>
2025-07-29 16:28:48 +0530

    made serverless mode

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import { compare, genSalt, hash } from "bcrypt";
-import express from "express"
+import express from "express";
 import { connectDB } from "./config/mongoDB.js";
 import dotenv from "dotenv";
 import router from "./routes/LoginRoute.js";
 import jwt from "jsonwebtoken";
-import cors from "cors"
+import cors from "cors";
+import serverless from "serverless-http";
 
 dotenv.config();
+
 const app = express();
 
 app.use(cors({
@@ -15,10 +17,18 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
 app.use("/", router);
 
-connectDB().then(()=>{
-    app.listen(3000);
-    console.log("connected successfully");
-})
+let isConnected = false;
+
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+    console.log("MongoDB connected successfully");
+  }
+
+  return await serverless(app)(req, res);
+};
+
+export { handler };
