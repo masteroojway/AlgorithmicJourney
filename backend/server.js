96063350cba34475e63c8a69221a14407483788c
Ujjwal jain <f20230602@goa.bits-pilani.ac.in>
2025-08-10 13:38:57 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,16 @@ const app = express();
 const allowedOrigins = process.env.FRONTEND_URL?.split(",").map(url => url.trim()) || [];
 
 app.use(cors({
-  origin: "http://localhost:5173", // or wherever your frontend is hosted
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
-
 
 
 app.use(express.json());
